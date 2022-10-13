40afd9adfecea05b5d8b473ee54c475e8d163a30
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-10-13 23:35:49 +0800

    Add code to manipulate Struct with Array<Char>

--- bin/dart_exercise.dart
+++ bin/dart_exercise.dart
@@ -3,9 +3,8 @@ import 'package:dart_exercise/generated_bindings.dart';
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:ffigen/ffigen.dart';
-import 'dart:io' show Platform, Directory;
+import 'dart:io' show Platform, Directory, stdout;
 import 'package:path/path.dart' as path;
-
 
 typedef twist_function = Void Function(Pointer<config>);
 
@@ -13,23 +12,48 @@ typedef TwistFunc = void Function(Pointer<config>);
 
 void main(List<String> arguments) {
   var libraryPath =
+      path.join(Directory.current.path, 'lib/c_struct', 'libtwist.so.1.0.0');
+  /*
+  // On MacOS:
+  var libraryPath =
       path.join(Directory.current.path, 'lib/c_struct', 'libtwist.1.0.0.dylib');
+  */
   final dylib = DynamicLibrary.open(libraryPath);
   final TwistFunc twist =
       dylib.lookup<NativeFunction<twist_function>>('twist').asFunction();
 
   final Pointer<config> c = calloc.call();
-  c.ref.id = 'hello 13579'.toNativeUtf8();
-  c.ref.pwd = 'world 24680'.toNativeUtf8();
+
+  int i;
+  final String id = 'hello 13579';
+  for (i = 0; i < id.length; i++) {
+    c.ref.id[i] = id.codeUnitAt(i);
+  }
+
+  final String pwd = 'world 24680';
+  for (i = 0; i < pwd.length; i++) {
+    c.ref.pwd[i] = pwd.codeUnitAt(i);
+  }
+
   c.ref.count = 3;
 
-  print(
-      'Before: id="${c.ref.id.toDartString()}"(${c.ref.id}), pwd="${c.ref.pwd.toDartString()}"(${c.ref.pwd}), count=${c.ref.count}');
+  stdout.writeln('Before:');
+  print_array(c.ref.id, 128);
+  print_array(c.ref.pwd, 128);
+  stdout.writeln('count: ${c.ref.count}');
 
   twist(c);
 
-  print(
-      'Twisted: id="${c.ref.id.toDartString()}"(${c.ref.id}), pwd="${c.ref.pwd.toDartString()}"(${c.ref.pwd}), count=${c.ref.count}');
+  stdout.writeln('Twisted:');
+  print_array(c.ref.id, 128);
+  print_array(c.ref.pwd, 128);
+  stdout.writeln('count: ${c.ref.count}');
+}
 
-  print('Hello world: ${dart_exercise.calculate()}!');
+void print_array(final Array<Char> a, int len) {
+  for (int i = 0; i < len && a[i] != 0; i++) {
+    stdout.write(a[i]);
+    stdout.write(' ');
+  }
+  stdout.write('\n');
 }
